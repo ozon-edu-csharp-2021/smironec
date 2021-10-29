1858ee4060216d262d359b9a847b4999141f74da
Sergey Mironets <mironets@company.dnevnik.ru>
2021-10-29 03:06:47 +0300

    feat: add http client

---
+++ src/OzonEdu.MerchandiseService.HttpClient/IMerchandiseHttpClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using OzonEdu.MerchandiseService.HttpModels;
+
+namespace OzonEdu.MerchandiseService.HttpClient
+{
+    public interface IMerchandiseHttpClient
+    {
+        Task<MerchOrderResponse> OrderMerch(long employeeId, MerchOrderViewModel merchOrderViewModel, CancellationToken token);
+        Task<IssuedMerchInfoResponse> GetIssuedMerchInfo(long employeeId, CancellationToken token);
+    }
+}
---
+++ src/OzonEdu.MerchandiseService.HttpClient/MerchandiseHttpClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using OzonEdu.MerchandiseService.HttpModels;
+
+namespace OzonEdu.MerchandiseService.HttpClient
+{
+    public class MerchandiseHttpClient : IMerchandiseHttpClient
+    {
+        private readonly System.Net.Http.HttpClient _httpClient;
+
+        public MerchandiseHttpClient(System.Net.Http.HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        
+        public async Task<MerchOrderResponse> OrderMerch(long employeeId, MerchOrderViewModel merchOrderViewModel, CancellationToken token)
+        {
+            var requestBody = JsonSerializer.Serialize(merchOrderViewModel);
+            var requestContent = new StringContent(requestBody, Encoding.UTF8, requestBody);
+            
+            using var response = await _httpClient.PostAsync($"v1/api/epmloyee/{employeeId}/merch", requestContent, token);
+            
+            var body = await response.Content.ReadAsStringAsync(token);
+            return JsonSerializer.Deserialize<MerchOrderResponse>(body);
+        }
+
+        public async Task<IssuedMerchInfoResponse> GetIssuedMerchInfo(long employeeId, CancellationToken token)
+        {
+            using var response = await _httpClient.GetAsync($"v1/api/employee/{employeeId}/merch/issued", token);
+            
+            var body = await response.Content.ReadAsStringAsync(token);
+            return JsonSerializer.Deserialize<IssuedMerchInfoResponse>(body);
+        }
+    }
+}
